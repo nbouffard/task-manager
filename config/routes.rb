b0429333927f8e89a1a84314be8b79ed59306446
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2024-01-29 12:51:40 +0100

    add create to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/task/:id', to: 'tasks#show', as: :task
   get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
 end
