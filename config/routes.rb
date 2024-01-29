8322a982bcef4d6792590cdcf4ca38b816fc0912
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2024-01-29 12:33:12 +0100

    add show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/tasks', to: 'tasks#index'
+  get '/task/:id', to: 'task#show', as: :task
 end
