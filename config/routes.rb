321fb15bdf2fbee776adcc06164d871d7c228450
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2024-01-29 13:05:06 +0100

    add delete to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
   get '/task/:id/edit', to: 'tasks#edit'
   patch '/task/:id', to: 'tasks#update'
+  delete '/task/:id', to: 'tasks#destroy'
 end
